a0eb523ca1d82cae5d4cfd8faa71308775a50ddf
Prajna Zhang <nihilism.z@gmail.com>
2011-08-13 19:15:26 +0800

    Tag observer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require jquery.tagsinput.min
+//= require articles
---
+++ app/models/tag_observer.rb
@@ -0,0 +1,9 @@
+class TagObserver < Mongoid::Observer
+  observe :article
+  
+  def after_save(record)
+    record.tags_array.each do |tag|
+      Tag.create name: tag
+    end
+  end
+end
