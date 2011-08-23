e208106a48f472aa9a9b2aae1011ffa4c9c150bf
Prajna Zhang <nihilism.z@gmail.com>
2011-08-23 17:44:09 +0800

    Add tests for relation observer

--- app/models/relation_observer.rb
+++ app/models/relation_observer.rb
@@ -1,7 +1,11 @@
 class RelationObserver < Mongoid::Observer
-  observe :post, :article
+  observe :post, :article, :post_comment, :article_comment
   
   def after_save(record)
-    "#{record.class.to_s}Relation".constantize.create user_id: record.user_id, receiver_id: record.id
+    if record.is_a?(Comment)
+      "#{record.class.name.gsub(/Comment/, '')}Relation".constantize.create user_id: record.user_id, receiver_id: record.sogoke_id
+    else
+      "#{record.class.name}Relation".constantize.create user_id: record.user_id, receiver_id: record.id
+    end
   end
 end
--- spec/models/relation_observer_spec.rb
+++ spec/models/relation_observer_spec.rb
@@ -4,11 +4,23 @@ describe RelationObserver do
   describe "post after save" do
     let(:observer) { RelationObserver.instance }
     let(:user) { Factory.create(:user) }
-    let(:post) { user.posts.create(title: "Hello", content: "World") }
+    let(:post) { user.posts.build(title: "Hello", content: "World") }
     
     it "creates relation between user and post" do
       PostRelation.should_receive(:create).with(user_id: user.id, receiver_id: post.id)
       observer.after_save(post)
+    end 
+  end
+  
+  describe "post comments after save" do
+    let(:observer) { RelationObserver.instance }
+    let(:user) { Factory.create(:user) }
+    let(:post) { user.posts.build(title: "Hello", content: "World") }
+    let(:comment) { user.post_comments.build(content: "World", sogoke_id: post.id) }
+    
+    it "creates relation between user and post" do
+      PostRelation.should_receive(:create).with(user_id: user.id, receiver_id: post.id)
+      observer.after_save(comment)
     end 
   end
 end
