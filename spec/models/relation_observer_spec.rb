9b8b29bbd06b0b3eb09134d3f5a6c7f3f4de73a4
Prajna Zhang <nihilism.z@gmail.com>
2011-08-23 16:16:48 +0800

    Relation observer test.

--- spec/models/relation_observer_spec.rb
+++ spec/models/relation_observer_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe RelationObserver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "post after save" do
+    let(:observer) { RelationObserver.instance }
+    let(:user) { Factory.create(:user) }
+    let(:post) { user.posts.create(title: "Hello", content: "World") }
+    
+    it "creates relation between user and post" do
+      PostRelation.should_receive(:create).with(user_id: user.id, receiver_id: post.id)
+      observer.after_save(post)
+    end 
+  end
 end
