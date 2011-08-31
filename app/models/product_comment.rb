b7ac7a32a58c1e7732f53828b095c61ee734ce62
Prajna Zhang <nihilism.z@gmail.com>
2011-08-31 17:09:06 +0800

    Product has no user but store

--- app/models/product_comment.rb
+++ app/models/product_comment.rb
@@ -2,6 +2,6 @@ class ProductComment < Comment
   belongs_to :product, :foreign_key => "about_id"
   
   def send_notification
-    notification! product.user.product_comment_notifications
+    notification! product.store.user.product_comment_notifications
   end
 end
